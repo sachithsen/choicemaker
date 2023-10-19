33b427ba10fe3ca15c4471d0f7d0a6243efb85de
Sachith Senarathne <sachithsenarathne@Sachiths-MacBook-Air.local>
2023-10-19 14:19:43 +0800

    Initial Commit

---
+++ src/main/java/org/sachith/choicemaker/ChoiceMakerApplication.java
@@ -0,0 +1,13 @@
+package org.sachith.choicemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiceMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sachith/choicemaker/ChoiceMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sachith.choicemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
